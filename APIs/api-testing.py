e3a54db46f3526ac69485edd51b58fdae9b5a53c
Sofija Zdjelar <sofija.zdjelar@outlook.com>
2023-01-03 14:15:04 +0100

    Delete APIs directory

--- APIs/api-testing.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-request = requests.get('http://api.open-notify.org')
-#print(request.status_code)
-
-people = requests.get('http://api.open-notify.org/astros.json')
-print(people.text)
